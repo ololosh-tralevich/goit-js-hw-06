9a64535a23c0871b1235024182aa1a898948b164
ololosh-tralevich <karpeko76@gmail.com>
2022-01-12 16:26:23 +0200

    upd

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+let ulElement = document.querySelector('ul#ingredients');
+
+const liArr = ingredients.map(ingredient => {
+  let liElement = document.createElement("li");
+  liElement.className = "item";
+  liElement.textContent = ingredient;
+  
+  return liElement;
+});
+
+ulElement.append(...liArr);
