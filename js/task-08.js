4c2e057b8e0b8f244becc564dd2b7bc1e6856bb4
ololosh-tralevich <karpeko76@gmail.com>
2022-01-14 15:26:37 +0200

    added HW 8, 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,6 +1,25 @@
 const formEl = document.querySelector("form.login-form");
-const userEmail = document.querySelector("form.login-form label");
-const userPassword = document.querySelectorAll("form.login-form");
-console.log(formEl);
-console.log(userEmail);
-console.log(userPassword);
+// const userEmail = formEl.elements.email.value;
+// const userPassword = formEl.elements.password;
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const userData = {
+    email: formEl.elements.email.value,
+    password: formEl.elements.password.value
+  };
+
+//   console.log(userData);
+//   console.log(formEl.elements.email.value);
+//   console.log(formEl.elements.password.value);
+
+  // if (login.value === "" || password.value === "") {
+  //     alert("Необходимо заполнить все поля!");
+  //     return;
+  // }
+
+//   console.log(`Login: ${email}, Passord ${password}`);
+     console.log(userData);
+
+  // event.currentTarget.reset();
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const widget = document.querySelector("div.widget");
+const colorMessage = document.querySelector("div.widget p span");
+const changeColorButton = document.querySelector("div.widget button");
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  widget.style.backgroundColor = randomColor;
+  colorMessage.textContent = randomColor;
+  console.log(randomColor);
 }
+
+changeColorButton.addEventListener("click", getRandomHexColor);
