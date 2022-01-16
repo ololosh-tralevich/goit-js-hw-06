ad57587c575ec798e6e192c475a22cb47e583a0c
ololosh-tralevich <karpeko76@gmail.com>
2022-01-16 19:59:11 +0200

    BUG FIX

--- js/task-06.js
+++ js/task-06.js
@@ -3,15 +3,6 @@ let inputField = document.querySelector("#validation-input");
 let lengthValue = Number(inputField.dataset.length);
 console.log(lengthValue);
 
-// inputField.addEventListener("blur", () => {
-//   if (inputField.value.length !== lengthValue) {
-//     inputField.className = "invalid";
-//     console.log(inputField.value.length);
-//   } else {
-//    inputField.className = "valid";
-//   }
-// });
-
 inputField.addEventListener("blur", () => {
   if (inputField.value.length === lengthValue) {
     return (inputField.className = "valid");
